7ddaf6bf1d12e1ececa31c668d8f5f740d67ad3a
Johannes Englisch <johannes_englisch@eva.mpg.de>
2024-08-27 12:24:06 +0200

    set up pyproject.toml

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
 from setuptools import setup
 
-
-setup(
-    name='cldfbench_sinnemakizeromarking',
-    py_modules=['cldfbench_sinnemakizeromarking'],
-    include_package_data=True,
-    zip_safe=False,
-    entry_points={
-        'cldfbench.dataset': [
-            'sinnemakizeromarking=cldfbench_sinnemakizeromarking:Dataset',
-        ]
-    },
-    install_requires=[
-        'cldfbench[glottolog]',
-        'pyyaml',
-    ],
-    extras_require={
-        'test': [
-            'pytest-cldf',
-        ],
-    },
-)
+setup()
